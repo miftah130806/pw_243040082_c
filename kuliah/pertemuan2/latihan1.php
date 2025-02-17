4fdc7370ea4d2b2cecc6898bbe3957a8ae27a0c0
miftah130806 <miftahmardiansah@gmail.com>
2025-02-17 16:55:56 +0700

    menyelesaikan latihan dan tugas 1

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba php</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1><?php echo"Hello World!";?> </h1>
+    
+    <h4>Nama Saya :<?php echo"MIftah MIrza"?></h4>
+</body>
+</html>
---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+    $nama = "Miftah Mirza";
+    $emai = "miftahmardiansah@gmail.com";
+    $pekerjaan = "mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "Universitas Pasundan";
+    echo "<h1>$nama</h1>";
+    echo "<p>$email</p>";
+    echo "halo nama saya $nama"
+?>  
